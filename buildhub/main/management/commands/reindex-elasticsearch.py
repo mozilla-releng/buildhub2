72d354b084d86b9e780a0b477dc8306726b6abbf
Peter Bengtsson <mail@peterbe.com>
2018-05-23 13:13:48 -0400

    Warn about _forcemerge, fixes #8

--- buildhub/main/management/commands/reindex-elasticsearch.py
+++ buildhub/main/management/commands/reindex-elasticsearch.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, you can obtain one at http://mozilla.org/MPL/2.0/.
 
+import requests
 from elasticsearch_dsl.connections import connections
 from elasticsearch.helpers import streaming_bulk
 from django.core.management.base import BaseCommand
@@ -19,7 +20,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        if settings.ES_BUILD_INDEX_SETTINGS['refresh_interval'] != '-1':
+        bulk_refresh_interval = (
+            settings.ES_BUILD_INDEX_SETTINGS['refresh_interval'] == '-1'
+        )
+        if not bulk_refresh_interval:
             print(" WARNING ".center(80, "-"))
             print(
                 "When doing re-index, it's adviced that you set "
@@ -33,19 +37,20 @@ class Command(BaseCommand):
         build_index.create()
 
         es = connections.get_connection()
-        report_every = 100
+        report_every = 500
         count = 0
 
         iterator = Build.objects.all().order_by("created_at")
         total_count = iterator.count()
 
+        index_name = settings.ES_BUILD_INDEX
         for success, doc in streaming_bulk(
             es,
             (
                 m.to_search().to_dict(True)
                 for m in iterator
             ),
-            index=settings.ES_BUILD_INDEX,
+            index=index_name,
             doc_type="doc",
         ):
             if not success:
@@ -57,3 +62,16 @@ class Command(BaseCommand):
                     "\t",
                     "{:.1f}%".format(100 * count / total_count)
                 )
+
+        if bulk_refresh_interval:
+            # We have done a bunch of bulk inserts into Elasticsearch with
+            # the refresh interval "disabled". Now we need to "force merge"
+            # the index. See
+            # https://www.elastic.co/guide/en/elasticsearch/reference/6.x/indices-update-settings.html#bulk
+            es_url = settings.ES_URLS[0]
+            force_merge_url = (
+                f"{es_url}/{index_name}/_forcemerge?max_num_segments=5"
+            )
+            response = requests.post(force_merge_url)
+            response.raise_for_status()
+            print("Force merged Elasticsearch index after bulk insert")
