2182a9c5fe37ad9963346789c82585e9f0a32507
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-01-13 10:09:17 -0800

    Add missing utils file

---
+++ tests/utils.py
@@ -0,0 +1,28 @@
+import os
+from django.conf import settings
+from google.cloud import bigquery
+
+# this is slow, so we evaluate this once and return the result
+try:
+    bigquery.Client()
+    BIGQUERY_AUTHENTICATED = True
+except:
+    # google.auth.exceptions.DefaultCredentialsError
+    BIGQUERY_AUTHENTICATED = False
+
+
+def runif_bigquery_testing_enabled(func):
+    """A decorator that will skip the test if the current environment is not set up for running tests.
+        @runif_bigquery_testing_enabled
+        def test_my_function_that_uses_bigquery(table_fixture):
+            ...
+    """
+    # importing this at module scope will break test discoverability
+    import pytest
+
+    bigquery_testing_enabled = (
+        os.environ.get("GOOGLE_APPLICATION_CREDENTIALS") and BIGQUERY_AUTHENTICATED
+    )
+    return pytest.mark.skipif(
+        not bigquery_testing_enabled, reason="requires valid GCP credentials"
+    )(func)
